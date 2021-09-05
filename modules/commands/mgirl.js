2cc30fe72341820f72fe832f80b9fd137bb5a15b
Your Name <you@example.com>
2021-09-06 00:12:43 +0700

    first commit

---
+++ modules/commands/mgirl.js
@@ -0,0 +1,24 @@
+﻿const fs = require("fs");
+module.exports.config = {
+	name: "1mgirl",
+	version: "1.0.1",
+	hasPermssion: 0,
+	credits: "CUONG",
+	description: "music",
+	commandCategory: "video",
+	usages: "noprefix",
+	cooldowns: 5,
+};
+module.exports.event = function ({ api, event }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("1mgirl") == 0 || (event.body.indexOf("1Mgirl") == 0)) {
+		var msg = {
+			body: "khối mgirl hiện có 2 video(Toppic: a,b,c,d,e,f,g,i,k,m,n,p,q,r,s,t,u,y,z)",
+			attachment: fs.createReadStream(__dirname + `/videogirl/mgirl/1.mp4`)
+		}
+		return api.sendMessage(msg, threadID, messageID);
+	}
+}
+module.exports.run = function ({ api, event, client, __GLOBAL }) {
+
+}
